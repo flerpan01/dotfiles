5268da1f3cedf0ae42be7e9bfb5afe55938c2f45
Andrey Hoglund <andrey.hoglund@protonmail.com>
2024-09-30 15:57:13 +0200

    added dot to dotfiles path

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ echo '# ~~~~~~~~~ flerpan dotfiles config ~~~~~~~~~~~~ #' >> $SH
 
 for file in shell/*; do
 	echo "${file}"
-	echo "source ${HOME}/dotfiles/${file}" >> $SH
+	echo "source ${HOME}/.dotfiles/${file}" >> $SH
 done
 
