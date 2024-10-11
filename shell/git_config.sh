850ac1df099b522c60378d01addf06e0ce0a5024
Andrey Hoglund <andrey.hoglund@protonmail.com>
2024-10-11 22:54:05 +0200

    same typo

--- shell/git_config.sh
+++ shell/git_config.sh
@@ -6,7 +6,7 @@ alias gl='git log --oneline --all --decorate --graph'
 alias gc='git commit -m'
 alias gca='git commit --amend'
 alias gcan='git commit --amend --no-edit'
-alias gb='git branch --format='%(HEADe %(color:cyan)%(refname:short)%(color:reset) - %(contents:subject) %(color:blue)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate
+alias gb='git branch --format="%(HEAD) %(color:cyan)%(refname:short)%(color:reset) - %(contents:subject) %(color:blue)(%(committerdate:relative)) [%(authorname)]" --sort=-committerdate'
 
 # make the prompt fancy:
 # + add git branch
