dc7b58b801a57c77f74255d74e264a1933113532
Andrey Hoglund <andrey.hoglund@protonmail.com>
2025-01-31 22:47:54 +0100

    Change git branch color to cyan and remove dead-space

--- shell/git_prompt.sh
+++ shell/git_prompt.sh
@@ -1,10 +1,10 @@
 # works with .bashrc
 if [ -f "${HOME}/.bashrc" ]; then
   parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+    git symbolic-ref --short HEAD 2> /dev/null | sed -E 's/(.+)/ (\1)/g'
   }
 
-  PS1="[\u@\h \W] \[\033[01;31m\]\$(parse_git_branch)\[\033[00m\] $ "
+  PS1="[\u@\h \W]\[\033[01;36m\]\$(parse_git_branch)\[\033[00m\] $ "
 else
 # works with .zshrc
 #if [ -f "${HOME}/.zshrc" ]; then
