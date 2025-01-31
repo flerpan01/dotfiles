86d2c13683c287e09cc63dc9126ae780098e2a0c
Andrey Hoglund <andrey.hoglund@protonmail.com>
2025-01-31 21:34:08 +0100

    Only check for .bashrc

--- shell/git_prompt.sh
+++ shell/git_prompt.sh
@@ -5,12 +5,9 @@ if [ -f "${HOME}/.bashrc" ]; then
   }
 
   PS1="[\u@\h \W] [\033[01;31m\]\$(parse_git_branch)\[\033[00m\] $ "
-fi
-
-
-
+else
 # works with .zshrc
-if [ -f "${HOME}/.zshrc" ]; then
+#if [ -f "${HOME}/.zshrc" ]; then
   parse_git_branch() {
     git symbolic-ref --short HEAD 2> /dev/null | sed -E 's/(.+)/ (\1)/g'
   }
