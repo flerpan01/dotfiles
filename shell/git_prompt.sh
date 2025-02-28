c49188fd32d0aa97e004884b4b05a3ec18403d3c
Andrey Hoglund <andrey.hoglund@protonmail.com>
2025-02-28 13:08:38 +0100

    Remove space in gitprompt

--- shell/git_prompt.sh
+++ shell/git_prompt.sh
@@ -4,7 +4,7 @@ if [ -f "${HOME}/.bashrc" ]; then
     git symbolic-ref --short HEAD 2> /dev/null | sed -E 's/(.+)/ (\1)/g'
   }
 
-  PS1="[\u@\h \W]\[\033[01;36m\]\$(parse_git_branch)\[\033[00m\] $ "
+  PS1="[\u@\h \W]\[\033[01;36m\]\$(parse_git_branch)\[\033[00m\]$ "
 else
 # works with .zshrc
 #if [ -f "${HOME}/.zshrc" ]; then
