8f9fbc24e59e6da699a552f79eedaca0546ddf80
Andrey Hoglund <andrey.hoglund@protonmail.com>
2025-03-14 10:02:08 +0100

    Fix typo

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -7,7 +7,7 @@ if [ -f "${HOME}/.bashrc" ]; then
   PS1="[\u@\h \W]\[\033[01;36m\]\$(parse_git_branch)\[\033[00m\]$ "
 else
 # works with .zshrc
-if [ -f "${HOME}/.zshrc" ]; then
+#if [ -f "${HOME}/.zshrc" ]; then
   setopt PROMPT_SUBST
   PROMPT='%c%F{cyan}$(parse_git_branch)%f $ '
 fi
